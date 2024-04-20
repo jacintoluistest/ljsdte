285598e4306adeaef47b4158e8130114a8156666
jacintoluistest <luisj81@gmail.com>
2024-04-19 20:24:35 -0600

    first files

---
+++ src/main/java/com/ljacinto/sdte/sdtedemo/SdtedemoApplication.java
@@ -0,0 +1,13 @@
+package com.ljacinto.sdte.sdtedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdtedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdtedemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ljacinto/sdte/sdtedemo/SdtedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljacinto.sdte.sdtedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdtedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
